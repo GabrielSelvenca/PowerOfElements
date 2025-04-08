8762513a13dbd145efd8ee453e762f086bc87801
Gsszs <gabriel.selvenca@icloud.com>
2025-04-08 15:55:50 -0300

    feat: Movimentação + jump + colisões (quase) prontas

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,77 @@
+using System;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float velocity = 10.0f;
+    public float speed = 7f;
+    public float sensitivity = 2f;
 
-    void Update()
+    public float jumpForce = 6f;
+
+    public LayerMask groundLayer;
+    private bool isGroundedFlag = false;
+    private bool jumpRequest;
+
+    private Rigidbody rb;
+    private CapsuleCollider capsule;
+    private Transform playerCamera;
+    private float rotationX = 0f;
+
+    void Start()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        rb = GetComponent<Rigidbody>();
+        capsule = GetComponent<CapsuleCollider>();
+        playerCamera = Camera.main.transform;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
-        Vector3 dir = new Vector3 (x, 0, y) * velocity;
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
 
-        transform.Translate(dir * Time.deltaTime);
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+        playerCamera.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGroundedFlag)
+        {
+            jumpRequest = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        if (jumpRequest)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpRequest = false;
+        }
+
+        Vector3 movement = transform.right * moveX + transform.forward * moveZ;
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if ((groundLayer.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            isGroundedFlag = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if ((groundLayer.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            isGroundedFlag = false;
+        }
     }
 }
